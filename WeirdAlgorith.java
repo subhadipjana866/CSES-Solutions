fd907f314e77b00e78bf9b144499c5b384d39802
Subhadip <subhadipjana866@gmail.com>
2023-12-09 21:48:34 +0530

    comments

--- WeirdAlgorith.java
+++ WeirdAlgorith.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class WeirdAlgorith {
     public static void compute(long n){
-        if(n==1){
+        if(n==1){  //If n==1 then directly print the value of n
             System.out.println(n);
-        } else if(n!=1){
-            if (n%2 == 0) {
+        } else if(n!=1){ //If n is not equal to 1 then apply the logic according the question
+            if (n%2 == 0) {  
             System.out.print(n+" ");
             n = n/2;
             compute(n);
@@ -18,9 +18,21 @@ public class WeirdAlgorith {
         
     }
     public static void main(String args[]){
+
+        //Initializing the Scanner object
+
         Scanner sc = new Scanner(System.in);
+
+        // Using long variable as the value of n can become very large
+
         long n = sc.nextLong();
+
+        //Closing the Scanner instance
+
         sc.close();
+
+        //Calling the compute function with n
+
         compute(n);
     }
 }
