6f3799e295245f28dbaf841163a5ed717b3a01a5
Subhadipjana866 <subhadipjana866@gmail.com>
2023-12-10 13:59:45 +0530

    Revert "comments"
    This reverts commit fd907f314e77b00e78bf9b144499c5b384d39802.

--- MissingNumber.java
+++ MissingNumber.java
@@ -3,19 +3,12 @@ import java.util.Scanner;
 public class MissingNumber {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int[] ar = new int[sc.nextInt()]; 
-        int[] hash = new int[ar.length]; //Initializing a hash array.
+        int[] ar = new int[sc.nextInt()];
+        int[] hash = new int[ar.length];
         for(int i=0;i<ar.length-1;i++){
             ar[i] = sc.nextInt();
         }
         sc.close();
-
-        //The idea is :
-        // Traverse the input array
-        // For each array element assign 1 at that location in the hash table
-
-        // Then return the position which have 0 value in the hash table
-
         for(int i=0;i<ar.length;i++){
             if(ar[i] != 0){
                 hash[ar[i] - 1] = 1;
--- Repetitions.java
+++ Repetitions.java
@@ -4,16 +4,9 @@ public class Repetitions {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-
-        //Initialize two integers with 1
-
         int ans = 1;
         int temp = 1;
         sc.close();
-
-        //Checking if the subsequent characters if they are same or not
-        //If same then increase temp
-        //Compare temp and ans for max value for each step 
 
         for(int i=1;i<s.length();i++){
             if(s.charAt(i) == s.charAt(i-1)){
