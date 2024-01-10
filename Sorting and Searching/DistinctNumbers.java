f6603182000d8ce420168c4e8a9ff1df6dcfabc0
Subhadipjana866 <subhadipjana866@gmail.com>
2024-01-10 21:11:48 +0530

    1st

---
+++ Sorting and Searching/DistinctNumbers.java
@@ -0,0 +1,25 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DistinctNumbers {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            while (i<n-1 && arr[i] == arr[i+1]) {
+                i++;
+            }
+            j++;
+        }
+        System.out.println(j);
+    }
+}
