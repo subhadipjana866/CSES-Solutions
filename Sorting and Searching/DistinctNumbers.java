0425ffa71de83caf458e6ccf2d36b693415ed8ce
Subhadipjana866 <subhadipjana866@gmail.com>
2024-01-23 20:50:18 +0530

    sns 1

--- Sorting and Searching/DistinctNumbers.java
+++ Sorting and Searching/DistinctNumbers.java
@@ -14,12 +14,17 @@ public class DistinctNumbers {
         Arrays.sort(arr);
 
         int j = 0;
-        for (int i = 0; i < n; i++) {
-            while (i<n-1 && arr[i] == arr[i+1]) {
-                i++;
+        for (int i = 1; i < n; i++) {
+            // while (i<n-1 && arr[i] == arr[i+1]) {
+            //     i++;
+            // }
+            // j++;
+            if (arr[i] != arr[i-1]) {
+                j++;
             }
-            j++;
         }
+        j++;
         System.out.println(j);
+        sc.close();
     }
 }
